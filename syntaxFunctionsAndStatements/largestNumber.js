a6ff810de1bd57f1bbd32a86a7d7807060d7f4ab
Daniel <danieldelchev1301@abv.bg>
2022-05-22 13:26:05 +0300

    learn

---
+++ syntaxFunctionsAndStatements/largestNumber.js
@@ -0,0 +1,12 @@
+function largestNumber(first, second, third) {
+    let result;
+    if (first > second && first > third) {
+        result = first;
+    } else if (second > first && second > third) {
+        result = second;
+    } else {
+        result = third;
+    }
+    console.log(`The largest number is ${result}.`);
+}
+largestNumber(5, -3, 16);
