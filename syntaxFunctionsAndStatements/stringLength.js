55ca197771b1ddc5b4863b44c7807554641e0251
Daniel <danieldelchev1301@abv.bg>
2022-05-25 11:33:33 +0300

    learn

---
+++ syntaxFunctionsAndStatements/stringLength.js
@@ -0,0 +1,6 @@
+function stringLength(first, second, third) {
+    let result = first.length + second.length + third.length;
+    console.log(result);
+    console.log(Math.floor(result / 3));
+}
+stringLength('chocolate', 'ice cream', 'cake');
