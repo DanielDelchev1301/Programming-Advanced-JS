daf1df176c43febb20f23af53f6a88aff5d4a124
Daniel <danieldelchev1301@abv.bg>
2022-06-09 22:35:18 -0400

    learn

---
+++ arraysAndNestedArrays/lastKNumberSequence.js
@@ -0,0 +1,18 @@
+function lastKNumbersSequence(n, k) {
+    let arr = [];
+    let currArr = [];
+
+    for (let i = 0; i < n; i++) {
+        if (i < 2) {
+            arr.push(1);
+            continue;
+        }
+        currArr = arr.slice(-k);
+        let sum = 0;
+        for (let num of currArr) {
+            sum += Number(num);
+        }
+        arr.push(sum);
+    }
+    return arr;
+}
