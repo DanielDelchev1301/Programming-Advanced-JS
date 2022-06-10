2642d59171f713b39fc8997b15d9633a63250711
Daniel <danieldelchev1301@abv.bg>
2022-06-09 22:36:03 -0400

    learn

---
+++ arraysAndNestedArrays/negativePositiveNumbers.js
@@ -0,0 +1,12 @@
+function negativeOrPositive(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        if (num < 0) {
+            newArr.unshift(arr[i]);
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    console.log(newArr.join('\n'));
+}
