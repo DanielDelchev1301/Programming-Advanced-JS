ef9a61e29870b87fe7cacebfc38b207784cc1117
Daniel <danieldelchev1301@abv.bg>
2022-06-30 21:26:30 -0400

    learn

---
+++ DOMIntroduction/Lab/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let readMoreElement = document.querySelector('#more');
+    readMoreElement.style.display = 'none';
+    let showTextElement = document.querySelector('#text');
+    showTextElement.style.display = 'inline';
+}
