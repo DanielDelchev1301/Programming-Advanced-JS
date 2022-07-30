7e74d77f99722b445829b15e18aae01b57ce13a1
Daniel <danieldelchev1301@abv.bg>
2022-07-29 21:28:31 -0400

    learn

---
+++ advancedFunctions/filterEmployees.js
@@ -0,0 +1,27 @@
+function filterEmployees(input, criteria) {
+    let [key, value] = criteria.split('-');
+    JSON.parse(input)
+        .filter(x => x[key] == value)
+        .forEach((x, i) => console.log(`${i}. ${x.first_name} ${x.last_name} - ${x.email}`));
+}
+filterEmployees(`[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  },  
+{
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }]`,
+    'gender-Female');
