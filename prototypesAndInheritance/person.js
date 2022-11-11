b43eff1cd87a3fad94aaefac199f1ff3d2e1a124
Daniel Delchev <kdanieldelchev1301@abv.bg>
2022-11-11 10:41:36 +0200

    salution

---
+++ prototypesAndInheritance/person.js
@@ -0,0 +1,24 @@
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    Object.defineProperty(this, 'fullName', {
+        set: function(value) {
+            fullName = value;
+            let [first, last] = fullName.split(' ');
+            this.firstName = first;
+            this.lastName = last;
+        },
+        get: function() {
+            return `${this.firstName} ${this.lastName}`;
+        }
+    });
+}
+let person = new Person("Peter", "Ivanov");
+console.log(person.fullName); //Peter Ivanov
+person.firstName = "George";
+console.log(person.fullName); //George Ivanov
+person.lastName = "Peterson";
+console.log(person.fullName); //George Peterson
+person.fullName = "Nikola Tesla";
+console.log(person.firstName); //Nikola
+console.log(person.lastName); //Tesla
